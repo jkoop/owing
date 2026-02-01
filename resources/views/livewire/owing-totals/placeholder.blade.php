136582ef645059204c9c67950aebe25948ad9b1b
Joe Koop <joe@joekoop.com>
2026-01-31 18:23:08 -0600

    Refactor user retrieval in placeholder view to use injected users collection

--- resources/views/livewire/owing-totals/placeholder.blade.php
+++ resources/views/livewire/owing-totals/placeholder.blade.php
@@ -1,5 +1,5 @@
 <ul>
-	@foreach (App\Models\User::where('id', '!=', Auth::id())->orderBy('name')->get() as $user)
+	@foreach ($users as $user)
 		<li class="opacity-50"><x-user :user="$user" /> <x-spinner /></li>
 	@endforeach
 </ul>
